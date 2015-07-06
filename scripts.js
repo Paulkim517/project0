c07f4d6a0a10f3b8473af05e71aa791debbe8f91
Paul Kim <paulkim517@gmail.com>
2015-07-06 00:59:40 -0700

    reverted back to local storage and changed header background black and text white.

--- scripts.js
+++ scripts.js
@@ -1,79 +1,70 @@
-$(function(){ 
-
- // form to create new rant
- var $myBlog = $('#save-book');
-  // underscore template
- var template = _.template($("#blog-template").html());
- // element to hold our list of rants
- var $blogSpot = $('.newBlog');
-
-// constructor for rant
-function Blog (userName, rant) {
+// constructor for blog
+function Blog(userName, rant) {
   this.userName = userName;
   this.rant = rant;
-};
-
-//holds all new instances of Rant
-Blog.all = [];
-
-//saves all new rants to the array 
-Blog.prototype.save = function(){
-  Blog.all.push(this);
-  console.log(this);
-};
-
-//renders underscore template
-Blog.prototype.render = function() {
-  var $rant = $(template(this))
-  $blogSpot.append($myBlog)
-  console.log("suck it twice"); 
+  this.items = localStorage.getItem("blogs");
+  this.key = "blogs";
+  this.items_json = JSON.parse(this.items);
 }
 
+// render function to fill out information to blog
+function SaveRender(){}
+
+SaveRender.prototype.saveToLs = function(obj) {
+   this.items_json; // var item =
+  if (this.items) {
+    item = JSON.parse(this.items);
+  } else {
+    item = [];
+  }
+
+  item.push(obj);
+  localStorage.setItem(this.key, JSON.stringify(item));
+}
+
+SaveRender.prototype.renderTemplate = function(template_source, where) {
+  // var items_json = JSON.parse(this.items);
+  var template = _.template($(template_source).html());
+
+  _.each(this.items_json, function(item) {
+    $(where).append(template(item));
+  });
+}
+
+Blog.prototype = new SaveRender();
+Blog.prototype.constructor = Blog;
+
+
+
+
+$(document).ready(function(){
+
+  // form to create new todo
+   var $myBlog = $('#save-book');
+
+  // element to hold our list of todos
+  var $blogSpot = $('#blog-template');
+
+   // underscore template
+   var template = _.template($("#blog-template").html());
+    console.log(template)
+
   // submit form to create new todo
-    $myBlog.on('submit', function(event) {
-     event.preventDefault();  
-     console.log("yeap")
-    // create new rants object from form data
-    var newAuthor = $('#author').val();
-    var newRant = $("#rant").val();
-    var myBlog = new Blog(newAuthor, newRant);
+    $myBlog.on('click', function(event) {
+     event.preventDefault();
     
-    //saves rant
-    myBlog.save();
 
-    //render rant
-    myBlog.render();
+    // create new todo object from form data
+    var newNewUser = $('#author').val();
+     console.log("suckt it")
+    var newRant = $("#rant").val();
+    var myBlog = new Blog(newNewUser,newRant);
+    myBlog.saveToLs(myBlog);
+    myBlog.renderTemplate("#blog-template", "#blog-container");
 
-   //  // reset the form
-   // $myBlog[0].reset();
-
-
-    });
+  
+  });
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
